2cb75921badc4eb230d5e9d9c5e6837c313f3324
yash-devop <kamble1234meena@gmail.com>
2025-02-02 15:50:46 +0530

    add: auth routes for google auth

--- server/src/routes/AuthRoutes.ts
+++ server/src/routes/AuthRoutes.ts
@@ -1,7 +1,48 @@
 import express from "express";
 import passport from "passport";
+import { FRONTEND_URL } from "../config/constants";
+
 export const authRouter = express.Router();
 
-authRouter.get('/', passport.authenticate("google", {
+authRouter.get(
+  "/google",
+  passport.authenticate("google", {
     scope: ["profile", "email"],
-  }));
+  })
+);
+
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `${FRONTEND_URL}/home`,
+    failureRedirect: `${FRONTEND_URL}/login2`,
+  }),
+  (req, res) => {
+    res.redirect(301, `${FRONTEND_URL}/home`);
+  }
+);
+
+authRouter.get("/profile", (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.status(401).json({
+      message: "Please login first.",
+    });
+  }
+  console.log("req", req.user);
+  res.json(req.user);
+});
+
+authRouter.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        console.error("Error destroying session:", err);
+        res.status(500).json({ message: "Failed to log out" });
+      }
+      res.clearCookie("connect.sid"); 
+      res.status(200).json({ message: "Logout successful" });
+    });
+  } else {
+    res.status(200).json({ message: "No active session to log out" });
+  }
+});
