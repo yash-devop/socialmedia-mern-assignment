cc57105c60654eb78e39ac10be596aee94090816
yash-devop <kamble1234meena@gmail.com>
2025-02-02 21:56:44 +0530

    add: export s3 client

--- server/src/config/aws.ts
+++ server/src/config/aws.ts
@@ -1,9 +1,3 @@
-import aws from "aws-sdk";
+import {S3Client} from "@aws-sdk/client-s3";
 
-aws.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
-
-export const s3 = new aws.S3();
+export const s3Client = new S3Client();
