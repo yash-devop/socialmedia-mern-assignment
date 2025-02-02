0a3cacce883e10afa82a49450ae3bc2b9187a213
yash-devop <kamble1234meena@gmail.com>
2025-02-02 21:55:13 +0530

    add: svg logo

---
+++ client/src/components/icons/Logo.tsx
@@ -0,0 +1,7 @@
+export const Logo=({
+    className
+}:{className?:string})=>{
+    return (
+        <svg role="img" className={className} style={{transform: "scale(1.4)"}} width="16" height="18" viewBox="0 0 16 18" fill="#ff6719" strokeWidth="1.8" stroke="none" xmlns="http://www.w3.org/2000/svg"><g><title></title><path d="M16 4H0V6H16V4Z"></path><path d="M0 8V18L7.9993 13.534L16 18V8H0Z"></path><path d="M16 0H0V2H16V0Z"></path></g></svg>
+    )
+}
