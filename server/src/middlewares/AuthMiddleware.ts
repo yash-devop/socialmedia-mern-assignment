a178ca32fa743d8ebb8a31920fd134d7c283ace9
yash-devop <kamble1234meena@gmail.com>
2025-02-02 21:55:34 +0530

    add: auth middleware

---
+++ server/src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ message: "Unauthorized" });
+};
