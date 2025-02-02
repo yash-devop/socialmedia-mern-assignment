82b5e19427bc83dab1f377afe970b990a717b1da
yash-devop <kamble1234meena@gmail.com>
2025-02-02 15:50:57 +0530

    add: post schema

---
+++ server/src/models/PostModel.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model("Post", postSchema);
