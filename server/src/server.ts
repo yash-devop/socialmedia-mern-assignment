4fbc938b8a6c423bd029f5ccdd3a35476165027f
yash-devop <kamble1234meena@gmail.com>
2025-02-02 15:51:28 +0530

    add: express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,56 @@
-import { connectMongoDB } from "./config/db";
 import express, { Request, Response } from "express";
 import passport from "passport";
-import "./config/passport";
+import session from "express-session";
+import { connectMongoDB } from "./config/db";
 import { authRouter } from "./routes/AuthRoutes";
-
+import "./config/passport"; // i imported the passport config here... it will run when server get started.
+import cors from "cors"
+import { config } from "dotenv";
+import MongoStore from "connect-mongo";
+config({
+  path:".env"
+})
 connectMongoDB();
 
 const app = express();
+
+// corss setup =>
+app.use(cors({
+  credentials: true,
+  origin: ["http://localhost:3000","http://localhost:5173"],
+}))
+
+// Middleware setup =>
 app.use(express.json());
-const apiRouter = express.Router();
+app.use(session({
+  secret: process.env.SECRET_KEY!,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+    httpOnly: true, // for security
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    sameSite: "strict", // same-site behavior
+  },
+  store: MongoStore.create({
+    mongoUrl : process.env.DATABASE_URL,
+    collectionName: "sessions"
+  })
+}));
+
+// Initialize Passport =>
 app.use(passport.initialize());
 app.use(passport.session());
+
+const apiRouter = express.Router();
+
+// Use the authRouter for authentication-related routes
 app.use("/api", apiRouter);
 
-apiRouter.get("/auth/google", authRouter);
-apiRouter.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  (req, res) => {
-    res.redirect("/dashboard");
-  }
-);
-
-apiRouter.get('/profile', (req: Request, res: Response) => {
-  if (!req.isAuthenticated()) {
-    res.status(401).json({
-        message: "Please login first."
-    })
-  }
-  res.json(req.user); // Return user profile if authenticated
-});
+// Mount the authRouter under "/api/auth"
+apiRouter.use("/auth", authRouter);
 
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log("Server started on", PORT);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
