17f4b2c347505cf72475f200917e7ac8a5a4d4d6
yash-devop <kamble1234meena@gmail.com>
2025-02-02 21:54:06 +0530

    add: react app router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, BrowserRouter as Router , Routes } from "react-router-dom"
+import Profile from "./components/Profile"
+import NotFoundPage from "./components/NotFound"
+import { AuthProvider } from "./context/AuthContext"
+import HomePage from "./pages/Home"
+import AuthProtected from "./components/AuthProtected"
+import LoginPage from "./components/auth/Login"
+import LandingPage from "./pages/LandingPage"
+import RootLayout from "./layout"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/"  element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<RootLayout />}>
+            <Route path="home" element={<HomePage />}/>
+            <Route path="profile" element={<AuthProtected><Profile /></AuthProtected>} />
+          </Route>
+          {/* <Route path="/home" element={<AuthProtected><HomePage /></AuthProtected>} /> */}
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </AuthProvider>
+    </Router>
   )
 }
 
