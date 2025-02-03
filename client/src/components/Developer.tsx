16955079cc95e8cac2b91ddb1331281499c8ae25
yash-devop <kamble1234meena@gmail.com>
2025-02-03 23:14:33 +0530

    add: developed by yash

---
+++ client/src/components/Developer.tsx
@@ -0,0 +1,7 @@
+export default function Developer(){
+    return (
+        <a href={"https://portfolio.yashstack.com/"} target="_blank" className="flex items-center justify-center rounded-lg fixed bottom-2 right-2 text-sm text-white p-3 bg-primary">
+            <p>Developed By — YashKamble</p>
+        </a>
+    )
+}
