f06d208d886f9b01881f27c3d59d30a38fc5f5ec
yash-devop <kamble1234meena@gmail.com>
2025-02-03 23:15:29 +0530

    add: not found page

---
+++ client/src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage(){
+    return (
+        <>
+            <div>
+                <p className="text-white">Not found</p>
+            </div>
+        </>
+    )
+}
