4c9d25f13428bb2f8b9792b16938affce0cfc1c6
yash-devop <kamble1234meena@gmail.com>
2025-02-04 09:29:49 +0530

    chore: add prod url

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -1,3 +1,4 @@
-export const FRONTEND_URL="http://localhost:5173"
+export const FRONTEND_URL="https://socialmedia.frontend.yashstack.com"
+// export const FRONTEND_URL="http://localhost:5173"
 // export const BACKEND_URL="http://localhost:8000"
 export const BACKEND_URL="https://socialmedia.api.yashstack.com"
